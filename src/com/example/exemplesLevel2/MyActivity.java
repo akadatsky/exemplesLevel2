ed061ca5b145819fb62b35118078e31d9430d630
andrey.kadatsky <andrey.kadatsky@gmail.com>
2015-03-25 18:14:54 +0200

    initial commit

---
+++ src/com/example/exemplesLevel2/MyActivity.java
@@ -0,0 +1,14 @@
+package com.example.exemplesLevel2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
